629c60f9e2f7a53ae08688f61e8d7c00e9408998
Frank <fcberni@hotmail.com>
2020-11-20 12:15:29 -0500

    Adding beginning Flask code

---
+++ app.py
@@ -0,0 +1,13 @@
+#Import dependencies
+from flask import Flask
+
+#Creating New Flask App Instance
+app = Flask(__name__)
+
+#Creating Flask Routes
+@app.route('/')
+def hello_world():
+    return 'Hello world'
+
+if __name__ == "__main__":
+    app.run(debug=True)
